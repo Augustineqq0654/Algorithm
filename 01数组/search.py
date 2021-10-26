c930d9fec31d210cf5b008c23e78dcb6027af3d9
Augustineqq0654 <39545316+Augustineqq0654@users.noreply.github.com>
2021-10-26 20:38:57 +0800

    Create search.py

---
+++ 01数组/search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            temp = left+(right-left)//2
+            if nums[temp] == target:
+                return(temp)
+            elif nums[temp] > target:
+                right = temp-1
+            else:
+                left = temp+1
+        else:
+            return -1
