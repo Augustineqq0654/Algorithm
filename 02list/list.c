748827a9f96e3159923c792b14afe658ceb8c7e6
kezihao <15927241986@163.com>
2023-01-21 20:11:33 +0800

    添加链表节点、尾插、输出 c 代码

---
+++ 02list/list.c
@@ -0,0 +1,44 @@
+#include "list.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//创建链表节点
+t_list *create_list_node(int val)
+{
+    t_list *p = malloc(sizeof(t_list));
+    if(p==NULL)
+    {
+        printf("malloc error!\n");
+        return NULL;
+    }
+    //清零
+    memset(p, 0, sizeof(t_list));
+    p->val = val;
+    p->next = NULL;
+    return p;
+}
+
+//链表节点尾插
+void tail_insert(t_list* phead, t_list* new_node)
+{
+    t_list *p = phead;
+    while(p->next!=NULL)
+    {
+        p = p->next;
+    }
+    p->next = new_node;
+}
+
+//打印链表节点数据
+void print_node(t_list* phead)
+{
+    t_list *p = phead;
+    p = phead->next;
+    while(p->next!=NULL)
+    {
+        printf("%d\t",p->val);
+        p = p->next;
+    }
+    printf("%d\n", p->val);
+}
---
+++ 02list/list.h
@@ -0,0 +1,11 @@
+
+struct list_node{
+    int val;
+    struct list_node* next;
+};
+typedef struct list_node t_list;
+
+//函数声明
+t_list *create_list_node(int val);
+void tail_insert(t_list* phead, t_list* new_node);
+void print_node(t_list* phead);
