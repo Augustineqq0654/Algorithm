cc8ecff587f711b726256880a7b84f17839c2ff8
kezihao <15927241986@163.com>
2023-01-31 22:17:42 +0800

    添加hash解法（需要仔细理解）

--- 03hash/two_sum.c
+++ 03hash/two_sum.c
@@ -8,6 +8,58 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <uthash.h>
+
+struct hash_table{
+    int key;
+    int val;
+    UT_hash_handle hh;
+};
+
+struct hash_table* hashtable;
+
+struct hash_table* find(int ikey)
+{
+    struct hash_table* tmp;
+    HASH_FIND_INT(hashtable, &ikey, tmp);
+    return tmp;
+}
+
+void insert(int ikey, int ival)
+{
+    struct hash_table* it = find(ikey);
+    if(it==NULL)
+    {
+        struct hash_table* tmp = malloc(sizeof(struct hash_table));
+        tmp->key = ikey, tmp->val = ival;
+        HASH_ADD_INT(hashtable, key, tmp);
+    }
+    else
+    {
+        it->val = ival;
+    }
+}
+
+int* two_sum_hash(int* nums, int nums_size, int target, int* return_size)
+{
+    hashtable = NULL;
+    for(int i=0; i<nums_size; i++)
+    {
+        struct hash_table* it = find(target-nums[i]);
+        if(it!=NULL)
+        {
+            int* ret = malloc(sizeof(int)*2);
+            ret[0] = it->val, ret[1] = i;
+            *return_size = 2;
+            return ret;
+        }
+        insert(nums[i], i);
+    }
+    *return_size = 0;
+    return NULL;
+}
+
+
 
 int *two_sum(int *nums, int nums_size, int target, int *return_size)
 {
@@ -28,13 +80,14 @@ int *two_sum(int *nums, int nums_size, int target, int *return_size)
     return NULL;
 }
 
+
 int main()
 {
-    int nums[] = {2, 7, 11, 15};
+    int nums[] = {2, 11, 7, 15};
     int target = 9;
     int nums_size = sizeof(nums)/sizeof(int);
     int return_size;
-    int *return_nums = two_sum(nums, nums_size, target, &return_size);
+    int *return_nums = two_sum_hash(nums, nums_size, target, &return_size);
     for(int i=0; i<return_size; i++)
     {
         printf("%d\t", return_nums[i]);
