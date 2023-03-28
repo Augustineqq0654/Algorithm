07e365bdfe9592a40b92176abf36849dc92bdc5e
Augustine <39545316+Augustineqq0654@users.noreply.github.com>
2023-03-28 18:47:56 +0800

    不适用临时变量交换两个数

---
+++ c_example/12swap_int.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    a = 11;
+    b = 99;
+    
+    printf("交换之前 - \n a = %d, b = %d \n\n", a, b);
+    
+    // 不使用临时变量交换两个整数的值
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    
+    printf("交换后 - \n a = %d, b = %d \n", a, b);    
+    
+}
