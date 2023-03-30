6238fe7c01a4ce3478b810876276bf272848004c
kezihao <15927241986@163.com>
2023-03-30 23:06:14 +0800

    is leap

---
+++ c_example/18is_leap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// (四年一闰，百年不闰) || 四百年再闰
+int main()
+{
+    int year;
+
+    printf("输入年份：");
+    scanf("%d", &year);
+
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100!= 0))
+        printf("%d 是闰年", year);
+    else
+        printf("%d 不是闰年", year);
+
+    return 0;
+}
