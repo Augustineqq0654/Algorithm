561e1e927ce6bb7921f9927e997db2a2b9a73875
kezihao <15927241986@163.com>
2023-04-09 21:08:51 +0800

    triangle

--- c_example/34triangle.c
+++ c_example/34triangle.c
@@ -26,5 +26,38 @@ int main()
         printf("\n");  // blank line before next row.
     }
 
+    //金字塔
+    int space, k=0;
+    for(i=1; i<=rows; ++i,k=0)
+    {
+        for(space=1; space<=rows-i; ++space)
+            printf("  ");
+
+        while(k != 2*i-1)
+        {
+            printf("* ");
+            ++k;
+        }
+
+        printf("\n");
+    }
+
+    // 杨辉三角
+    int coef=1;
+    for(i=0; i<rows; i++)
+    {
+        for(space=1; space<=rows-i; space++)
+            printf("  ");
+        for(j=0; j<=i; j++)
+        {
+            if(j==0 || i==0)
+                coef=1;
+            else
+                coef=coef*(i-j+1)/j;
+            printf("%4d", coef);
+        }
+        printf("\n");  // blank line between rows.
+    }
+
     return 0;
 }
