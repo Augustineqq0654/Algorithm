6edb9806da833dafd21408e51b3cafd0da35f3dd
Augustine <39545316+Augustineqq0654@users.noreply.github.com>
2023-11-08 21:06:55 +0800

    Create sort.h

---
+++ myCode/include/sort.h
@@ -0,0 +1,12 @@
+#ifndef __SORT_H_
+#define __SORT_H_
+
+void Swap(int *a, int *b);
+void Display(int array[], int length);
+void InsertSort(int array[], int length);
+void MySort(int array[], int length);
+void BubbleSort(int array[], int length);
+void SelectSort(int array[], int length);
+void QuickSort(int array[], int length);
+
+#endif  // SORT_H_
